61105dbb81c4767dbb0fc3d3fc221c782897de49
paraIncog <selgeandreas02@gmail.com>
2023-06-11 11:02:48 +0300

    let app use cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('./config');
- 
+var cors = require('cors')
 
 const app = express();
 const port = 3000;
@@ -15,6 +15,24 @@ app.get('/', (req, res) => {
     res.send('Hello');
 });
  
+// var express = require('express')
+
+var corsOptions = {
+  origin: 'https://andrease-matkad-app.onrender.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/api/treks', cors(corsOptions), async (req, res, next) => {
+  const rows = await pool.query(
+    'SELECT * FROM treks;'
+);
+  res.json(rows)
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port')
+})
+
 app.get('/api/treks', async (req, res) => {
   try {
     const rows = await pool.query(
