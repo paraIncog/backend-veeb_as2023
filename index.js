ccd00845dd61e102228b10f5e63ce78b0e930078
paraIncog <selgeandreas02@gmail.com>
2023-06-12 15:33:31 +0300

    Fix index

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ var cors = require('cors')
 
 const app = express();
 const port = 3030;
+app.use(express.json());
 
 // app.use(cors({
 //   origin: '*'
 // }));
 
-app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+  res.send('Simple API homepage');
 });
- 
+
 // var express = require('express')
 
 var corsOptions = {
@@ -22,21 +22,11 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+
 app.get('/api/treks', cors(corsOptions), async (req, res, next) => {
-  const rows = await pool.query(
-    'SELECT * FROM treks;'
-);
-  res.json(rows)
-})
-
-app.listen(port, function () {
-  console.log('CORS-enabled web server listening on port')
-})
-
-app.get('/api/treks', async (req, res) => {
   try {
-    const rows = await pool.query(
-        'SELECT * FROM treks;'
+    const { rows } = await pool.query(
+      'SELECT * FROM treks;'
     );
     res.json(rows);
   } catch (error) {
@@ -44,11 +34,11 @@ app.get('/api/treks', async (req, res) => {
     res.status(500).send(error.message)
   }
 });
- 
+
 app.post('/api/treks', (req, res) => {
   res.status(201).send('Sent the new data to the DB ...')
 });
- 
-// app.listen(port, () => {
-//     console.log("Server running on port", port);
-// });
+
+app.listen(port, () => {
+  console.log('CORS-enabled web server listening on port')
+})
